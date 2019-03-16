9879b161b9953e868f69b88dc275b9db9770920c
Ha Van Son <havanson@gmail.com>
2019-03-16 11:01:13 +0700

    Updates

---
+++ precache-manifest.6d732486f93aef4855803a4a47bb5b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f62c85ca877c059f99b2",
+    "url": "/absentmanger/static/css/main.58d1c5a3.chunk.css"
+  },
+  {
+    "revision": "f62c85ca877c059f99b2",
+    "url": "/absentmanger/static/js/main.6e57cbf6.chunk.js"
+  },
+  {
+    "revision": "61916ee614023ac2ee71",
+    "url": "/absentmanger/static/js/runtime~main.39b44488.js"
+  },
+  {
+    "revision": "b9ba13c0fa1748fa2c56",
+    "url": "/absentmanger/static/css/2.b5dad3d0.chunk.css"
+  },
+  {
+    "revision": "b9ba13c0fa1748fa2c56",
+    "url": "/absentmanger/static/js/2.ec63f1ce.chunk.js"
+  },
+  {
+    "revision": "d6ec51092cf70228a345fdcba55bcf6a",
+    "url": "/absentmanger/index.html"
+  }
+];
