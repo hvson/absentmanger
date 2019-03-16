9f2815cd9e792eaf8db580411df744dd642a5ec3
Ha Van Son <havanson@gmail.com>
2019-03-16 10:54:24 +0700

    initial

---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Consumer } from '../../context';
+import axios from 'axios';
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = () => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
+
+  onDeleteClick = async (id, dispatch) => {
+    try {
+      await axios.delete('https://jsonplaceholder.typicode.com/users/${id}');
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    } catch (error) {
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    }
+  };
+
+  render() {
+    const { id, name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+
+          return (
+            <div className="card car-body mb-3">
+              <h4>
+                {name}{' '}
+                <i
+                  onClick={this.onShowClick}
+                  className="fas fa-sort-down"
+                  style={{ cursor: 'pointer' }}
+                />
+                <i
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  className="fas fa-times"
+                  style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                />
+                <Link to={`contact/edit/${id}`}>
+                  <i
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: 'pointer',
+                      float: 'right',
+                      color: 'Blue',
+                      marginRight: '1 em'
+                    }}
+                  />
+                </Link>
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default Contact;
